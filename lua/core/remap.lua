994c8e586efb8686a0fc9761cd307260a5619d2b
0xsj <sjtommylee@gmail.com>
2024-08-17 11:20:27 -0700

    new

---
+++ lua/core/remap.lua
@@ -0,0 +1,25 @@
+local opts = { noremap = true, silent = true }
+local keymap = vim.keymap
+
+vim.g.mapleader = " "
+
+
+-- menu
+keymap.set("n", "<leader>`", vim.cmd.Ex)
+
+-- exit insert mode,
+keymap.set("i", "jk", "<Esc>")
+
+
+-- format
+keymap.set("n", "<leader>fm", vim.lsp.buf.format)
+
+-- increment, decrement
+keymap.set("n", "+", "<C-a>")
+keymap.set("n", "-", "<C-x>")
+
+-- delete word backwards
+keymap.set("n", "dw", 'vb"_d')
+
+-- select all
+keymap.set("n", "<C-a>", "gg<S-v>G")
